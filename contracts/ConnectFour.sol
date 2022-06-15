28631189ecfcfe788dccb96b25b3d723b275ad90
Hagen <hagen.hoferichter@gmail.com>
2022-06-15 20:18:22 +0200

    Added claim. Basic functions implemented

--- contracts/ConnectFour.sol
+++ contracts/ConnectFour.sol
@@ -1,19 +1,82 @@
-pragma solidity 0.8.14;
+pragma solidity ^0.8.7;
+/**
+ * @dev Contract module that helps prevent reentrant calls to a function.
+ *
+ * Inheriting from `ReentrancyGuard` will make the {nonReentrant} modifier
+ * available, which can be applied to functions to make sure there are no nested
+ * (reentrant) calls to them.
+ *
+ * Note that because there is a single `nonReentrant` guard, functions marked as
+ * `nonReentrant` may not call one another. This can be worked around by making
+ * those functions `private`, and then adding `external` `nonReentrant` entry
+ * points to them.
+ *
+ * TIP: If you would like to learn more about reentrancy and alternative ways
+ * to protect against it, check out our blog post
+ * https://blog.openzeppelin.com/reentrancy-after-istanbul/[Reentrancy After Istanbul].
+ */
+abstract contract ReentrancyGuard {
+    // Booleans are more expensive than uint256 or any type that takes up a full
+    // word because each write operation emits an extra SLOAD to first read the
+    // slot's contents, replace the bits taken up by the boolean, and then write
+    // back. This is the compiler's defense against contract upgrades and
+    // pointer aliasing, and it cannot be disabled.
 
-contract ConnectFourV1 {
+    // The values being non-zero value makes deployment a bit more expensive,
+    // but in exchange the refund on every call to nonReentrant will be lower in
+    // amount. Since refunds are capped to a percentage of the total
+    // transaction's gas, it is best to keep them low in cases like this one, to
+    // increase the likelihood of the full refund coming into effect.
+    uint256 private constant _NOT_ENTERED = 1;
+    uint256 private constant _ENTERED = 2;
+
+    uint256 private _status;
+
+    constructor() {
+        _status = _NOT_ENTERED;
+    }
+
+    /**
+     * @dev Prevents a contract from calling itself, directly or indirectly.
+     * Calling a `nonReentrant` function from another `nonReentrant`
+     * function is not supported. It is possible to prevent this from happening
+     * by making the `nonReentrant` function external, and make it call a
+     * `private` function that does the actual work.
+     */
+    modifier nonReentrant() {
+        // On the first call to nonReentrant, _notEntered will be true
+        require(_status != _ENTERED, "ReentrancyGuard: reentrant call");
+
+        // Any calls to nonReentrant after this point will fail
+        _status = _ENTERED;
+
+        _;
+
+        // By storing the original value once again, a refund is triggered (see
+        // https://eips.ethereum.org/EIPS/eip-2200)
+        _status = _NOT_ENTERED;
+    }
+}
+
+pragma solidity ^0.8.7;
+
+contract ConnectFourV1 is ReentrancyGuard{
 	/// ERRORS ///
 	error InvalidMove();
 	error Unauthorized();
 	error GameFinished();
 	error InvalidStake();
+	error InvalidRewards();
 
 	/// EVENTS ///
 	event GameStarted(uint256 gameId);
 	event MovePerformed(uint256 gameId, uint256 moveId, uint8 col, Color color);
 	event GameWon(uint256 gameId, Color color);
 	event GameDraw(uint256 gameId);
-	event Stake(address addr, uint256 value, Color color);
-	event RewardsCalculated(uint256 gameId, uint256 winColorAmount, uint256 rewardAmount, uint256 treasuryAmt);
+	event StakeRed(address addr, uint256 value, Color color, uint256 totalAmount);
+	event StakeBlue(address addr, uint256 value, Color color, uint256 totalAmount);
+	event RewardsCalculated(uint256 gameId, uint256 winColorAmount, uint256 rewardAmount, uint256 treasuryAmt, Color color);
+	event Claim(address sender, uint256 epoch, uint256 addedReward);
 
 	struct Game {
 		uint64 height;
@@ -26,10 +89,6 @@ contract ConnectFourV1 {
 		uint256 winColorAmount;
 		uint256 rewardAmount;
 	}
-
-	bool gameActive = false;
-	address owner;
-	uint256[7] internal columnVotes = [0, 0, 0, 0, 0, 0, 0]; 
 
     enum Color {
         Red,
@@ -44,10 +103,13 @@ contract ConnectFourV1 {
         bool claimed; // default false
     }
 
-    mapping(uint256 => mapping(address => UserRound)) public ledger;
+	mapping(uint256 => mapping(address => UserRound)) public ledger;
 	mapping(uint256 => Game) public games;
-    mapping(address => uint256[]) public userStakeGames;
+	mapping(address => uint256[]) public userStakeGames;
 
+	bool public gameActive = false;
+	address public owner;
+	uint256[7] internal columnVotes = [0, 0, 0, 0, 0, 0, 0]; 
 	uint64 internal constant topColumn = 283691315109952;
 	uint64 internal constant maxHeight = 418861572486;
 	uint8 internal constant gameFee = 1;
@@ -82,6 +144,13 @@ contract ConnectFourV1 {
 		_;
 	}
 
+	// Checks that caller is no contract
+	modifier notContract() {
+        require(!_isContract(msg.sender), "Contract not allowed");
+        require(msg.sender == tx.origin, "Proxy contract not allowed");
+        _;
+    }
+
 	constructor() {
 		moveStartBlockNr = block.number;
 			Game memory game = Game({
@@ -102,6 +171,7 @@ contract ConnectFourV1 {
 	}
 
 	function transferOwnership (address newOwner) public onlyOwner {
+		require(newOwner != address(0), 'Cannot be NULL.');
 		owner = newOwner;
 	}
 
@@ -112,6 +182,14 @@ contract ConnectFourV1 {
 	function unpause() public onlyOwner {
 		gameActive = true;
 	}
+
+    function _isContract(address account) internal view returns (bool) {
+        uint256 size;
+        assembly {
+            size := extcodesize(account)
+        }
+        return size > 0;
+    }
 
 	function _executeMove() internal {
 		// Load game state
@@ -131,8 +209,8 @@ contract ConnectFourV1 {
 		// WINNING MOVE
 		if (color_wins) {
 			game.finished = true;
-			_calculateRewards(globalGameId);
 			emit GameWon(globalGameId, Color(game.moves & 1));
+			_calculateRewards(globalGameId);
 			_startNewRound();
 		// DRAW
 		} else if (game.height == maxHeight) {
@@ -208,6 +286,7 @@ contract ConnectFourV1 {
 	public 
 	payable 
 	activeGame 
+	notContract
 	checkGameTick {
 		UserRound storage userRound = ledger[gameId][msg.sender];
 		require(msg.value >= minStakeAmount, "Stake amount too smol.");
@@ -221,17 +300,19 @@ contract ConnectFourV1 {
 			userStakeGames[msg.sender].push(gameId);
 			if (color == Color.Red){
 				game.redAmount += msg.value;
+				game.totalAmount += msg.value;
+				emit StakeRed(msg.sender, msg.value, color, game.totalAmount);
 			} else if (color == Color.Blue){
 				game.blueAmount += msg.value;
+				game.totalAmount += msg.value;
+				emit StakeBlue(msg.sender, msg.value, color, game.totalAmount);
 			} else {
 				revert InvalidStake();
 			}
-			game.totalAmount += msg.value;
-			emit Stake(msg.sender, msg.value, color);
 		}
 	}
 
-	function vote(uint256 gameId, uint8 col) public activeGame checkGameTick {
+	function vote(uint256 gameId, uint8 col) public activeGame notContract checkGameTick {
 		Game storage game = games[gameId];
 		require(!game.finished, "You voted for a finished game.");
 		UserRound memory userRound = ledger[gameId][msg.sender];
@@ -272,6 +353,19 @@ contract ConnectFourV1 {
 		;
 	}
 
+	function refundable(uint256 gameId, address user) public view returns (bool) {
+		UserRound memory userRound = ledger[gameId][user];
+		Game memory game = games[gameId];
+		if (game.finished) {
+			return false;
+		} 
+		return 
+			(!game.finished) && 
+			(game.moves == 41) &&
+			(userRound.amount != 0)
+		;
+	}
+
 	function _calculateRewards(uint256 gameId) internal {
         require(games[gameId].winColorAmount == 0 && games[gameId].rewardAmount == 0, "Rewards calculated");
         Game storage game = games[gameId];
@@ -279,27 +373,63 @@ contract ConnectFourV1 {
         uint256 treasuryAmt;
         uint256 rewardAmount;
         // Red wins
-        if (Color(game.moves & 1) == Color.Red) {
+        if ((game.moves & 1) == uint8(Color.Red)) {
             winColorAmount = game.redAmount;
             treasuryAmt = (game.totalAmount * gameFee) / 100;
             rewardAmount = game.totalAmount - treasuryAmt;
+			emit RewardsCalculated(gameId, winColorAmount, rewardAmount, treasuryAmt, Color.Red);
         }
         // Blue wins
-        if (Color(game.moves & 1) == Color.Blue) {
-				winColorAmount = game.blueAmount;
-				treasuryAmt = (game.totalAmount * gameFee) / 100;
-				rewardAmount = game.totalAmount - treasuryAmt;
+        if ((game.moves & 1) == uint8(Color.Blue)) {
+			winColorAmount = game.blueAmount;
+			treasuryAmt = (game.totalAmount * gameFee) / 100;
+			rewardAmount = game.totalAmount - treasuryAmt;
+			emit RewardsCalculated(gameId, winColorAmount, rewardAmount, treasuryAmt, Color.Blue);
         }
         // House wins
         else {
-            winColorAmount = 0;
-            rewardAmount = 0;
-            treasuryAmt = game.totalAmount;
+			revert InvalidRewards();
         }
         game.winColorAmount = winColorAmount;
         game.rewardAmount = rewardAmount;
         // Add to treasury
         treasuryAmount += treasuryAmt;
-        emit RewardsCalculated(gameId, winColorAmount, rewardAmount, treasuryAmt);
+    }
+
+	function claim(uint256[] calldata gameIds) external nonReentrant notContract {
+        uint256 reward; // Initializes reward
+
+        for (uint256 i = 0; i < gameIds.length; i++) {
+            require(gameIds[i] < globalGameId, "Round has not started.");
+            require(games[gameIds[i]].finished, "Round has not ended or is a draw.");
+
+            uint256 addedReward = 0;
+
+            // Round valid, claim rewards
+            if (games[gameIds[i]].finished) {
+                // require(claimable(gameIds[i], msg.sender), "Not eligible for claim");
+                Game memory game = games[gameIds[i]];
+                addedReward = (ledger[gameIds[i]][msg.sender].amount * game.rewardAmount) / game.winColorAmount;
+            }
+            // Round invalid, refund bet amount
+            else {
+                require(refundable(gameIds[i], msg.sender), "Not eligible for refund");
+                addedReward = ledger[gameIds[i]][msg.sender].amount;
+            }
+
+            ledger[gameIds[i]][msg.sender].claimed = true;
+            reward += addedReward;
+
+            emit Claim(msg.sender, gameIds[i], addedReward);
+        }
+
+        if (reward > 0) {
+            _safeTransfer(address(msg.sender), reward);
+        }
+    }
+
+	function _safeTransfer(address to, uint256 value) internal {
+        (bool success, ) = to.call{value: value}("");
+        require(success, "TransferHelper: BNB_TRANSFER_FAILED");
     }
 }

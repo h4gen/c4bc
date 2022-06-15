e1928f5c215cbf63ec02eeea78960bd59c2e9c57
Hagen <hagen.hoferichter@gmail.com>
2022-06-15 15:49:31 +0200

    added reward calculation

--- contracts/ConnectFour.sol
+++ contracts/ConnectFour.sol
@@ -1,10 +1,11 @@
-pragma solidity ^0.8.10;
+pragma solidity 0.8.14;
 
 contract ConnectFourV1 {
 	/// ERRORS ///
 	error InvalidMove();
 	error Unauthorized();
 	error GameFinished();
+	error InvalidStake();
 
 	/// EVENTS ///
 	event GameStarted(uint256 gameId);
@@ -12,35 +13,45 @@ contract ConnectFourV1 {
 	event GameWon(uint256 gameId, Color color);
 	event GameDraw(uint256 gameId);
 	event Stake(address addr, uint256 value, Color color);
+	event RewardsCalculated(uint256 gameId, uint256 winColorAmount, uint256 rewardAmount, uint256 treasuryAmt);
 
 	struct Game {
 		uint64 height;
 		uint64[2] board;
 		uint8 moves;
 		bool finished;
+		uint256 redAmount;
+		uint256 blueAmount;
+		uint256 totalAmount;
+		uint256 winColorAmount;
+		uint256 rewardAmount;
 	}
 
-	bool roundActive = false;
+	bool gameActive = false;
+	address owner;
 	uint256[7] internal columnVotes = [0, 0, 0, 0, 0, 0, 0]; 
 
     enum Color {
         Red,
-        Yellow
+        Blue
     }
 
 	struct UserRound {
         Color color;
         uint256 amount;
 		uint256 last_move;
-		bool exists;
+		bool exists;  // default false
         bool claimed; // default false
     }
 
     mapping(uint256 => mapping(address => UserRound)) public ledger;
 	mapping(uint256 => Game) public games;
-    mapping(address => uint256[]) public userGames;
+    mapping(address => uint256[]) public userStakeGames;
 
 	uint64 internal constant topColumn = 283691315109952;
+	uint64 internal constant maxHeight = 418861572486;
+	uint8 internal constant gameFee = 1;
+	uint256 internal treasuryAmount;
 	uint256 public globalGameId = 0;
 	uint256 public globalMoveId = 2;
 	uint256 internal minStakeAmount = 10**14;
@@ -49,6 +60,7 @@ contract ConnectFourV1 {
 	uint256 internal roundAmountRed;
 	uint256 internal roundAmountYello;
 
+	// Checks whether next move should be triggered.
 	modifier checkGameTick {
 		if ((block.number - moveStartBlockNr) <= blockDelta){
 			_;
@@ -58,39 +70,80 @@ contract ConnectFourV1 {
 		}
 	}
 
+	// Checks that caller is owner of contract.
+	modifier onlyOwner {
+		require(msg.sender == owner, "Not authorized.");
+		_;
+	}
+
+	// Checks that game is not paused.
+	modifier activeGame {
+		require(gameActive, "Game not running.");
+		_;
+	}
+
 	constructor() {
 		moveStartBlockNr = block.number;
 			Game memory game = Game({
 			height: uint64(0),
 			board: [uint64(0), uint64(0)],
 			moves: 0,
-			finished: false
+			finished: false,
+			redAmount: 0,
+			blueAmount: 0,
+			totalAmount: 0,
+			winColorAmount: 0,
+			rewardAmount: 0
 		});
 
 		games[globalGameId] = game;
+		gameActive = true;
+		owner = msg.sender;
+	}
+
+	function transferOwnership (address newOwner) public onlyOwner {
+		owner = newOwner;
+	}
+
+	function pause() public onlyOwner {
+		gameActive = false;
+	}
+
+	function unpause() public onlyOwner {
+		gameActive = true;
 	}
 
 	function _executeMove() internal {
+		// Load game state
 		Game storage game = games[globalGameId];
-		moveStartBlockNr = block.number;
+		// Get column with highest vote for current column
 		uint8 col = _getMaxVoteCol();
 		if (game.finished) revert GameFinished();
+		// Set bit on bitboard for new chip of color
 		game.board[game.moves & 1] ^= uint64(1) << ((game.height >> 6*col) & 63);
-		game.height += uint64(1) << 6*col;
+		// Check if this would be a valid move, revert otherwise.
 		if ((game.board[game.moves & 1] & topColumn) != 0) revert InvalidMove();
+		// Increment height bit range
+		game.height += uint64(1) << 6*col;
 		emit MovePerformed(globalGameId, globalMoveId,col, Color(game.moves & 1));
+		// Check if this was a winning move
 		bool color_wins = _checkColorWin(globalGameId, game.moves & 1);
+		// WINNING MOVE
 		if (color_wins) {
 			game.finished = true;
+			_calculateRewards(globalGameId);
 			emit GameWon(globalGameId, Color(game.moves & 1));
 			_startNewRound();
-		} else if (game.height == 418861572486) {
+		// DRAW
+		} else if (game.height == maxHeight) {
 			emit GameDraw(globalGameId);
 			_startNewRound();
+		// CONTINUE
 		} else {
-		columnVotes = [0, 0, 0, 0, 0, 0, 0]; 
-		game.moves++;
-		globalMoveId++;
+			moveStartBlockNr = block.number;
+			columnVotes = [0, 0, 0, 0, 0, 0, 0]; 
+			game.moves++;
+			globalMoveId++;
 		}
 	}
 
@@ -99,15 +152,21 @@ contract ConnectFourV1 {
 				height: uint64(0),
 				board: [uint64(0), uint64(0)],
 				moves: 0,
-				finished: false
+				finished: false,
+				redAmount: 0,
+				blueAmount: 0,
+				totalAmount: 0,
+				winColorAmount: 0,
+				rewardAmount: 0
 			});
 			games[++globalGameId] = newGame;
+			moveStartBlockNr = block.number;
 			emit GameStarted(globalGameId);
 	}
 
 	function _checkColorWin(uint256 gameId, uint8 side) public view returns (bool) {
 		uint64 board = games[gameId].board[side];
-		uint8[4] memory directions = [1, 7, 6, 8];
+		uint8[4] memory directions = [6, 8, 7, 1];
 		uint64 bb;
 		unchecked {
 			for (uint8 i = 0; i < 4; i++) {
@@ -145,21 +204,34 @@ contract ConnectFourV1 {
 		return levels;
 	}
 
-	function stake(uint256 gameId, Color color) public payable checkGameTick {
+	function stake(uint256 gameId, Color color) 
+	public 
+	payable 
+	activeGame 
+	checkGameTick {
 		UserRound storage userRound = ledger[gameId][msg.sender];
 		require(msg.value >= minStakeAmount, "Stake amount too smol.");
 		require(globalGameId <= gameId, "You tried to stake a finised round.");
 		if (userRound.amount > 0){
 			require(userRound.color == color, "You already staked another color this round.");
 		} else {
+			Game storage game = games[gameId];
 			userRound.color = color;
 			userRound.amount += msg.value;
-			userGames[msg.sender].push(gameId);
+			userStakeGames[msg.sender].push(gameId);
+			if (color == Color.Red){
+				game.redAmount += msg.value;
+			} else if (color == Color.Blue){
+				game.blueAmount += msg.value;
+			} else {
+				revert InvalidStake();
+			}
+			game.totalAmount += msg.value;
 			emit Stake(msg.sender, msg.value, color);
 		}
 	}
 
-	function vote(uint256 gameId, uint8 col) public checkGameTick {
+	function vote(uint256 gameId, uint8 col) public activeGame checkGameTick {
 		Game storage game = games[gameId];
 		require(!game.finished, "You voted for a finished game.");
 		UserRound memory userRound = ledger[gameId][msg.sender];
@@ -199,4 +271,35 @@ contract ConnectFourV1 {
 			(userRound.amount > 0)
 		;
 	}
+
+	function _calculateRewards(uint256 gameId) internal {
+        require(games[gameId].winColorAmount == 0 && games[gameId].rewardAmount == 0, "Rewards calculated");
+        Game storage game = games[gameId];
+        uint256 winColorAmount;
+        uint256 treasuryAmt;
+        uint256 rewardAmount;
+        // Red wins
+        if (Color(game.moves & 1) == Color.Red) {
+            winColorAmount = game.redAmount;
+            treasuryAmt = (game.totalAmount * gameFee) / 100;
+            rewardAmount = game.totalAmount - treasuryAmt;
+        }
+        // Blue wins
+        if (Color(game.moves & 1) == Color.Blue) {
+				winColorAmount = game.blueAmount;
+				treasuryAmt = (game.totalAmount * gameFee) / 100;
+				rewardAmount = game.totalAmount - treasuryAmt;
+        }
+        // House wins
+        else {
+            winColorAmount = 0;
+            rewardAmount = 0;
+            treasuryAmt = game.totalAmount;
+        }
+        game.winColorAmount = winColorAmount;
+        game.rewardAmount = rewardAmount;
+        // Add to treasury
+        treasuryAmount += treasuryAmt;
+        emit RewardsCalculated(gameId, winColorAmount, rewardAmount, treasuryAmt);
+    }
 }
